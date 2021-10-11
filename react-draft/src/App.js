222b219714b8e2f475bc4e27cd4ec319b0f9766e
APW <aungphyowai@techfun-myanmar.com>
2021-10-11 18:00:29 +0630

    Integrate Draft rich-text editor with html to react framework

---
+++ react-draft/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RichEditor from "./rich-text-editor";
+
+function App() {
+  return (
+    <div className="App">
+      <RichEditor />
+    </div>
+  );
+}
+
+export default App;
